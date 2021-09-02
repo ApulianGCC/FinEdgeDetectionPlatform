f5046d1323c0cc7f8aa80a1877891b437de960cd
KdLSosa <gabriele.colasuonno@gmail.com>
2021-09-02 17:59:50 +0200

    gestito output immagini

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
-from flask import Flask, flash, request, redirect, url_for, render_template
+from flask import Flask, flash, request, redirect, url_for, render_template, send_from_directory
 import eval
 import os
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = '/input/'
+UPLOAD_FOLDER = 'input/'
+OUTPUT_FOLDER = 'output/'
 ALLOWED_EXTENSIONS = {'jpg', 'jpeg'}
 
 app = Flask(__name__)
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['OUTPUT_FOLDER'] = OUTPUT_FOLDER
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -18,7 +20,7 @@ def allowed_file(filename):
 def main():
     return render_template("index.html", title="Test Falsk")
 
-@app.route("/output", methods=['GET', 'POST'])
+@app.route("/upload", methods=['GET', 'POST'])
 def output():
     if request.method == 'POST':
         # check if the post request has the file part
@@ -32,11 +34,19 @@ def output():
             flash('No selected file')
             return redirect(request.url)
         if file and allowed_file(file.filename):
-            print('ciao')
             filename = secure_filename(file.filename)
             file.save(app.config['UPLOAD_FOLDER'] + filename)
-            #return redirect(url_for('download_file', name=filename))
-    return render_template("index.html", title="Test Falsk")
+            eval.run()
+    return render_template("output.html", title="Test Falsk", filename=filename)
+
+@app.route('/input/<filename>')
+def send_image_input(filename):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], filename)
+
+@app.route('/output/<filename>')
+def send_image_output(filename):
+    return send_from_directory(app.config["OUTPUT_FOLDER"], filename.replace('jpg','png'))
 
 if __name__ == "__main__":
+    eval.load_model()
     app.run(debug=True, port=5000)
