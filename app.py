ad13df48bb784a8f817fd55d1f8da4ac768d7cba
KdLSosa <gabriele.colasuonno@gmail.com>
2021-09-06 13:40:18 +0200

    rimossa possibilità di caricare foto dopo il primo upload, aggiunta possibilità di eliminare foto caricata

--- app.py
+++ app.py
@@ -26,7 +26,6 @@ app.config[ 'DROPZONE_DEFAULT_MESSAGE'] = '<div class="dz-content">' \
                                           '<a class= "dz-link">Click to Upload</a>' \
                                           '</div>'
 
-
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
